fb47a83be4de289522d2f82df363ded72e9f31be
Adjuntor <10227133+Adjuntor@users.noreply.github.com>
2026-06-06 09:18:30 -0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,6 +2,14 @@ RSS_UPDATE_INTERVAL = 60  # seconds
 RSS_LAST_ARTICLE_RANGE = 7  # days
 MAX_CACHE_SIZE = 5000 # Maximum number of links to store
 
+DAILY_WEBHOOK_URL = "https://discord.com/api/webhooks/159999999999999928/asdasdgffhggdfhertsdzxvretwsvfxcrhvcxrtg" # Daily Message Webhook
+DAILY_MESSAGE = "This is your daily warning." # Daily Message
+DAILY_HOUR = 12 # Hour to send the message
+DAILY_MINUTE = 0 # Minute to send the message
+DAILY_SECOND = 0 # Second to send the message
+DAILY_MICROSECOND = 0 # Microsecond to send the message
+
+
 RSS_FEEDS = [
     {
         "url": "https://example.com/rss",
